28882ef1f31c8b778f143465cc923be236285017
TacoConKvass <128845692+TacoConKvass@users.noreply.github.com>
2023-09-06 14:53:30 +0200

    Upload Shotgun and Pistol reload mechanisms

---
+++ Arsenal/ShotgunAbility.cs
@@ -0,0 +1,40 @@
+public class Shotgun_Ability : AimedAbility_RL, IAttack, IAbility
+{
+    public override void Initialize(CastAbility_RL abilityController, CastAbilityType castAbilityType)
+	{
+		base.Initialize(abilityController, castAbilityType);
+		if (this.CanManuallyReload)
+		{
+			this.m_reloadAbility = AbilityLibrary.GetAbility(AbilityType.ShotgunReloadTalent);
+			this.m_reloadAbility = UnityEngine.Object.Instantiate<BaseAbility_RL>(this.m_reloadAbility);
+			this.m_reloadAbility.transform.SetParent(base.transform);
+			this.m_reloadAbility.Initialize(abilityController, castAbilityType);
+		}
+		this.m_gravityReductionModWhenAiming = this.GravityReduction;
+
+        //I didn't want to do it this way, but I don't remember how I did it last time
+		this.frameTimer = 0;
+	}
+
+    protected override void Update()
+	{
+		// Run normal code here, then add this at the end
+        // Didn't want to post it here, as I don't want to show too much of the code, as it's not my property
+        // And it's unnecessary to show it here
+
+		if (SaveManager.PlayerSaveData.CurrentCharacter.Weapon != AbilityType.ShotgunWeapon && base.CurrentAmmo == 0)
+		{
+            // Every game update the timer increases by one
+			this.frameTimer++;
+
+            // After 120 updates, reload the weapon
+			if (this.frameTimer == 120)
+			{
+				base.CurrentAmmo = base.MaxAmmo;
+				this.frameTimer = 0;
+			}
+		}
+	}
+
+    private int frameTimer;
+}
