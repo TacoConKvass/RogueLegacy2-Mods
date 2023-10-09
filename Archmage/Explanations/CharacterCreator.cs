1f9b74300c689636a7e03a23a091669cf4ff5267
TacoConKvass <128845692+TacoConKvass@users.noreply.github.com>
2023-10-09 16:34:51 +0200

    Archmage Mod

---
+++ Archmage/Explanations/CharacterCreator.cs
@@ -0,0 +1,32 @@
+public static void ArchmageRoll(CharacterData charData)
+{
+		AbilityType[] spells_available = CharacterCreator.GetAvailableSpells(ClassType.CURIO_SHOPPE_CLASS);
+
+		charData.Weapon = ((spells_available.Length != 0) ? spells_available[RNGManager.GetRandomNumber(RngID.Lineage, "GetRandomWeapon", 0, spells_available.Length)] : AbilityType.None);
+		spells_available.Remove(charData.Weapon);
+
+		charData.Talent = ((spells_available.Length != 0) ? spells_available[RNGManager.GetRandomNumber(RngID.Lineage, "GetRandomTalent", 0, spells_available.Length)] : AbilityType.None);
+		spells_available.Remove(charData.Talent);
+
+		charData.Spell = ((spells_available.Length != 0) ? spells_available[RNGManager.GetRandomNumber(RngID.Lineage, "GetRandomSpells", 0, spells_available.Length)] : AbilityType.None);
+		spells_available.Remove(charData.Spell);
+
+        charData.TraitTwo = TraitType.BonusMagicStrength; // Give the character Crippling Intellect
+}
+
+public static void GenerateRandomLook(CharacterData charData)
+{
+    //... Normal logic
+	if (charData.ClassType == ClassType.MagicWandClass || charData.ClassType == ClassType.AstroClass || charData.ClassType == ClassType.LuteClass)
+	{
+		CharacterCreator.ArchmageRoll(charData); // Reroll ability slots to only spells
+	}
+}
+
+public static void ApplyRandomizeKitTrait(CharacterData charData, bool randomizeSpell, bool excludeCurrentAbilities, bool useLineageSeed)
+{
+    if (charData.ClassType == ClassType.MagicWandClass || charData.ClassType == ClassType.AstroClass || charData.ClassType == ClassType.LuteClass)
+	{
+		CharacterCreator.ArchmageRoll(charData); // Reroll ability slots to only spells
+	}
+}
---
+++ Archmage/README.md
@@ -0,0 +1,25 @@
+# Welcome to Archmage Mod!
+Do you feel that the ability system of Rogue Legacy 2 is too complicated?
+Do you rarely feel the need to use your Weapon and prefer swinging Spells?
+I am happy to present to you, the Archmage mod!
+
+Mod requested by: @Gridd from the CDG Discord Server
+
+## This mod contains
+1. Characters have weapons. Just weapons. All ability slots are rerolled into a random weapon.
+
+## Notes
+1. Doesn't work well with True Rogue (still gives you just spells, but you don't get the Crippling Intellect Trait)
+2. For some reason doesn't work on Thanathophobias (I dunno why tho ¯\_(ツ)_/¯)
+
+## How to install?
+1. Download the file.
+2. Backup your "Assembly-CSharp.dll" file located in "GamePath/Rogue Legacy 2_Data/Managed".
+3. Replace the file with the modded one.
+4. Have fun!
+
+Parts of files with modified content can be found in the Explanations folder, with comments explaining some things
+
+# Patch Notes
+v 1.0
+- Release
